b2074e01724b85b43c0b85136e05c22c5caeda8d
mengjiu999 <1099399203@qq.com>
2017-12-25 22:01:47 +0800

    java

---
+++ 0006Recursive.java
@@ -0,0 +1,13 @@
+//递归调用
+public class Recursive{
+    public static void main(string arg[]){
+        System.out.println(method(5));
+    }
+//只有执行方法才占内存
+    public static int method(int n){
+        if(n == 1)
+            return 1;
+        else
+            return n*method(n-1);
+    }
+}
