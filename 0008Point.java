789aadd94742cf300a5cce661e80453ea3477b61
mengjiu999 <1099399203@qq.com>
2017-12-26 23:32:21 +0800

    java

---
+++ 0008Point.java
@@ -0,0 +1,28 @@
+public class Point{
+    double x,y,z;
+    Point(double _x,double _y,double _z)
+    x = _x;
+    y = _y;
+    z = _z;
+    }
+    void setX(double _x){
+        x = _x;
+    }
+    void setY(double _y){
+        y = _y;
+    }
+    void setZ(double _z){
+        z = _z;
+    }
+    double getDistance(Point p){
+        return (x - p.x)*(x - p.x) + (y - p.y)*(y - p.y) + (z - p.z)*(z - p.z);
+    }
+}
+public class TestPoint{
+    public static void main(String[] args){
+        Point p = new Point(1.0,3.0,3,0);
+        Point p1 = new Point(0.0,0.0,0.0);
+        System.out.println(p.getDistance(p1));
+        
+    }
+}
